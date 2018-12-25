f33e6e61130d68ba0d8010a32272caa5e8a5f740
Pavel Lobashov <shockwavenn@gmail.com>
2018-12-25 16:18:15 +0300

    Add manual install script for centos

---
+++ Centos7/install.sh
@@ -0,0 +1,36 @@
+# Cannot use docker since no systemd in centos images
+sudo su
+sudo yum update -y
+curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
+sudo cp nginx.repo /etc/yum.repos.d/nginx.repo
+sudo yum install nginx -y
+sudo cp nginx.conf /etc/nginx/nginx.conf
+sudo yum install epel-release -y
+
+sudo yum install postgresql postgresql-server -y
+sudo service postgresql initdb 
+sudo chkconfig postgresql on
+sudo sed -i "s/ident/trust/g" /var/lib/pgsql/data/pg_hba.conf
+sudo service postgresql restart
+
+cd /tmp
+sudo -u postgres psql -c "CREATE DATABASE onlyoffice;"
+sudo -u postgres psql -c "CREATE USER onlyoffice WITH password 'onlyoffice';"
+sudo -u postgres psql -c "GRANT ALL privileges ON DATABASE onlyoffice TO onlyoffice;"
+
+sudo yum install redis -y
+sudo service redis start
+sudo systemctl enable redis
+
+sudo yum install rabbitmq-server -y
+sudo service rabbitmq-server start
+sudo systemctl enable rabbitmq-server
+
+sudo yum -y install http://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
+sudo yum install onlyoffice-documentserver-ie -y
+
+sudo service supervisord start
+sudo systemctl enable supervisord
+sudo service nginx start
+sudo systemctl enable nginx
+
