ac82722d665f581f8c1a614051f8b56780cd131b
Pavel Lobashov <ShockwaveNN@gmail.com>
2019-06-20 16:19:11 +0300

    Correctly handle custom repo for centos (#18)
    * Correctly handle custom repo for centos
    
    * Add centos info
    
    * Add newline
    
    * Force specific edition on documentserver
    
    * Fail on any error
    
    * Return to previous directory

--- Centos7/install.sh
+++ Centos7/install.sh
@@ -1,4 +1,5 @@
 # Cannot use docker since no systemd in centos images
+set -e
 
 curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
 sudo cp nginx.repo /etc/yum.repos.d/nginx.repo
@@ -16,6 +17,7 @@ cd /tmp
 sudo -u postgres psql -c "CREATE DATABASE onlyoffice;"
 sudo -u postgres psql -c "CREATE USER onlyoffice WITH password 'onlyoffice';"
 sudo -u postgres psql -c "GRANT ALL privileges ON DATABASE onlyoffice TO onlyoffice;"
+cd -
 
 sudo yum install redis -y
 sudo service redis start
@@ -25,7 +27,12 @@ sudo yum install rabbitmq-server -y
 sudo service rabbitmq-server start
 sudo systemctl enable rabbitmq-server
 
-sudo yum -y install http://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
+if [ "$#" -ne 1 ]; then
+    sudo yum -y install http://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
+else
+    sudo cp onlyoffice.repo /etc/yum.repos.d/onlyoffice.repo
+fi
+
 sudo yum install onlyoffice-documentserver-ie -y
 
 sudo service supervisord start
--- README.md
+++ README.md
@@ -6,3 +6,19 @@ Install Documentserver on ubuntu using [manual](http://helpcenter.onlyoffice.com
 docker build -t onlyoffice-documentserver-install-from-manual **OPERATION_SYSTEM** && \
 docker run -itd -p 80:80 --name onlyoffice-documentserver-install-from-manual onlyoffice-documentserver-install-from-manual
 ```
+
+## Centos
+
+For checking release installation on CentOS use
+
+```bash
+cd Centos7
+bash install.sh
+```
+
+For checking custom branch edit `Centos7/onlyoffice.repo` file accordingly and run
+
+```bash
+cd Centos7
+bash install.sh custom-repo
+```
