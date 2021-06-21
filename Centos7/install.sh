075f2966e566c90ec4592df99e5e04a92c1d7acf
Pavel Lobashov <shockwavenn@gmail.com>
2021-06-21 18:05:57 +0300

    Actualize Centos installation script
    Now it uses single repo and require version to install

--- Centos7/install.sh
+++ Centos7/install.sh
@@ -3,6 +3,9 @@ set -e
 
 DS_PRODUCT=onlyoffice-documentserver-ee
 
+sudo yum -y install dnf
+sudo dnf install 'dnf-command(config-manager)'
+
 sudo cp nginx.repo /etc/yum.repos.d/nginx.repo
 sudo yum install nginx -y
 sudo cp nginx.conf /etc/nginx/nginx.conf
@@ -31,7 +34,7 @@ sudo systemctl enable rabbitmq-server
 if [ "$#" -ne 1 ]; then
     sudo yum -y install http://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
 else
-    sudo cp onlyoffice.repo /etc/yum.repos.d/onlyoffice.repo
+    sudo dnf config-manager --add-repo http://repo-doc-onlyoffice-com.s3.amazonaws.com/onlyoffice/repo-testing/centos/onlyoffice-testing.repo
 fi
 
 sudo yum install ${DS_PRODUCT} -y
