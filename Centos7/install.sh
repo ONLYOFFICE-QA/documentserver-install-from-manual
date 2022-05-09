f39e7317eae25c99369fad66239157e55a3accd5
Pavel Lobashov <ShockwaveNN@gmail.com>
2022-05-09 13:32:25 +0300

    Add fonts installation step for DocumentServer Centos (#34)
    * Add fonts installation step for DocumentServer Centos
    
    * Remove extra line

--- Centos7/install.sh
+++ Centos7/install.sh
@@ -32,6 +32,9 @@ sudo yum install rabbitmq-server -y
 sudo service rabbitmq-server start
 sudo systemctl enable rabbitmq-server
 
+sudo yum install cabextract xorg-x11-font-utils fontconfig
+sudo rpm -i https://deac-ams.dl.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
+
 if [ "$#" -ne 1 ]; then
     sudo yum -y install http://download.onlyoffice.com/repo/centos/main/noarch/onlyoffice-repo.noarch.rpm
     sudo yum install ${DS_PRODUCT} -y
